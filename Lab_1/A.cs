960cdf8cf2ba789787657d560afdda16f0ff2b6e
Denis Schifrin <56796561+Schidev@users.noreply.github.com>
2021-04-12 15:14:34 +0300

    Class A was modified

--- Lab_1/A.cs
+++ Lab_1/A.cs
@@ -8,47 +8,44 @@ namespace Lab_1
 {
     class A
     {
-		const int SIZE = 10;
+		private static int[] _array;
 
-		int[] arr = new int[SIZE];
-
-		public int GetSIZE()
+		public A(int[] array)
 		{
-			return SIZE;
-		}
-
-		public void FillArray()
-		{
-			for (int i = 0; i < SIZE; i++)
+			_array = new int[array.Length];
+			for (int i = 0; i < array.Length; i++)
 			{
-				arr[i] = Convert.ToInt32(Console.ReadLine());
+				//_array[i] = Convert.ToInt32(Console.ReadLine());
+				_array[i] = array[i];
 			}
 		}
 
 		public void Print()
 		{
-			for (int i = 0; i < SIZE; i++)
+			for (int i = 0; i < _array.Length; i++)
 			{
-				Console.Write("" + this.arr[i] + ", ");
+				if (i == 0) Console.Write("[" + _array[i] + ", ");
+				if ((i != 0) && (i != _array.Length - 1)) Console.Write(" " + _array[i] + ", ");
+				if (i == _array.Length - 1) Console.WriteLine(" " + _array[i] + "]");
 			}
 		}
 
 		public void Sort(int head, int tail)
 		{
 			int i = head, j = tail;
-			int pivot = this.arr[tail];
+			int pivot = _array[tail];
 
 			do
 			{
-				while (this.arr[i] < pivot) i++;
-				while (this.arr[j] > pivot) j--;
+				while (_array[i] < pivot) i++;
+				while (_array[j] > pivot) j--;
 
 				if (i <= j)
 				{
 					int temp;
-					temp = this.arr[i];
-					this.arr[i] = this.arr[j];
-					this.arr[j] = temp;
+					temp = _array[i];
+					_array[i] = _array[j];
+					_array[j] = temp;
 					i++;
 					j--;
 				}
@@ -58,5 +55,13 @@ namespace Lab_1
 			if (head < j) this.Sort(head, j);
 			if (tail > i) this.Sort(i, tail);
 		}
+
+
+
+		public int GetTail()
+		{
+			return _array.Length - 1;
+		}
 	}
+
 }
