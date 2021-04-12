950aad20f8beada18e27ea3d08f3f174ed13c9f9
Denis Schifrin <56796561+Schidev@users.noreply.github.com>
2021-04-12 13:51:45 +0300

    Add project files.

---
+++ Lab_1/A.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class A
+    {
+		const int SIZE = 10;
+
+		int[] arr = new int[SIZE];
+
+		public int GetSIZE()
+		{
+			return SIZE;
+		}
+
+		public void FillArray()
+		{
+			for (int i = 0; i < SIZE; i++)
+			{
+				arr[i] = Convert.ToInt32(Console.ReadLine());
+			}
+		}
+
+		public void Print()
+		{
+			for (int i = 0; i < SIZE; i++)
+			{
+				Console.Write("" + this.arr[i] + ", ");
+			}
+		}
+
+		public void Sort(int head, int tail)
+		{
+			int i = head, j = tail;
+			/*int head = Head;
+			int tail = Tail;*/
+			int pivot = this.arr[tail];
+
+			do
+			{
+				while (this.arr[i] < pivot) i++;
+				while (this.arr[j] > pivot) j--;
+
+				if (i <= j)
+				{
+					int temp;
+					temp = this.arr[i];
+					this.arr[i] = this.arr[j];
+					this.arr[j] = temp;
+					i++;
+					j--;
+				}
+
+			} while (i <= j);
+
+			if (head < j) this.Sort(head, j);
+			if (tail > i) this.Sort(i, tail);
+		}
+	}
+}
---
+++ Lab_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			A b = new A();
+			Console.WriteLine();
+			Console.WriteLine("Enter 10 numbers: ");
+			b.FillArray();
+
+			Console.WriteLine();
+			Console.Write("Array b4 sorting: ");
+			b.Print();
+
+			b.Sort(0, b.GetSIZE() - 1);
+
+			Console.WriteLine();
+			Console.Write("Array after sorting: ");
+			b.Print();
+
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine();
+		}
+    }
+}
